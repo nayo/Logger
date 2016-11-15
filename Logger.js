fa0f1a0e9c3d55159299fc0831eb8b7fa0dbdfee
Genaro Contreras Gutierrez <genaro_contreras@hotmail.com>
2016-11-14 16:28:13 -0800

    Update Logger.js

--- Logger.js
+++ Logger.js
@@ -2,42 +2,34 @@
     this.Logger={};
     if (!window.console) window.console = {};
     if (!window.console.log) window.console.log = function() {};
-    if (!window.console.warn) window.console.warn = function() {};
-    if (!window.console.error) window.console.error = function() {};
 
     var console_log = console.log;
-    var console_warn = console.warn;
-    var console_error = console.error;
 
     Logger.enableLog = function enableLogger() {
         console.log = console_log;
-        console.warn = console_warn;
-        console.error = console_error
     };
     Logger.disableLog = function disableLogger() {
         console.log = function() {};
-        console.warn = function() {};
-        console.error = function() {};
     };
 
     function parse(arguments) {
-        var string = "%c  ";
+        var string = "%c["+new Date().toLocaleString()+"]  ";
         for (var i = 0; i < arguments.length; i++) {
             string = string + arguments[i] + "  ";
         }
         return string;
     }
-    Logger.info = function() {
-        console.log(parse(arguments), 'border-radius:3px;background:#d9edf7;color: #31708f;border-color:#d6e9c6');
-    }
-    Logger.error = function() {
-        console.error(arguments);
-    }
-    Logger.warn = function() {
-        console.warn(arguments);
-    }
     Logger.success = function() {
-        console.log(parse(arguments), 'border-radius:3px;background:#dff0d8;color: #3c763d;border-color:#ebccd1');
+        console.log(parse(arguments), 'font-weight: bold;border-radius:3px;/*background:#dff0d8;*/color: #3c763d;border-color:#ebccd1');
+    }
+    Logger.info = function() {
+        console.log(parse(arguments), 'font-weight: bold;border-radius:3px;/*background:#d9edf7*/;color: #31708f;border-color:#d6e9c6');
+    }
+    Logger.warning = function() {
+        console.log(parse(arguments), 'font-weight: bold;border-radius:3px;/*background:#fcf8e3*/;color: #8a6d3b;border-color:#faebcc');
+    }
+    Logger.danger = function() {
+        console.log(parse(arguments), 'font-weight: bold;border-radius:3px;/*background:#f2dede*/;color: #a94442;border-color:#ebccd1');
     }
 
 }());
