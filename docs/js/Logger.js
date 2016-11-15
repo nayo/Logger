3a017339d654a32c288056e408011fc180624261
Genaro Contreras Gutierrez <genaro_contreras@hotmail.com>
2016-11-14 17:01:09 -0800

    Create Logger.js

---
+++ docs/js/Logger.js
@@ -0,0 +1,35 @@
+(function() {
+    this.Logger={};
+    if (!window.console) window.console = {};
+    if (!window.console.log) window.console.log = function() {};
+
+    var console_log = console.log;
+
+    Logger.enableLog = function enableLogger() {
+        console.log = console_log;
+    };
+    Logger.disableLog = function disableLogger() {
+        console.log = function() {};
+    };
+
+    function parse(arguments) {
+        var string = "%c["+new Date().toLocaleString()+"]  ";
+        for (var i = 0; i < arguments.length; i++) {
+            string = string + arguments[i] + "  ";
+        }
+        return string;
+    }
+    Logger.success = function() {
+        console.log(parse(arguments), 'font-weight: bold;border-radius:3px;/*background:#dff0d8;*/color: #3c763d;border-color:#ebccd1');
+    }
+    Logger.info = function() {
+        console.log(parse(arguments), 'font-weight: bold;border-radius:3px;/*background:#d9edf7*/;color: #31708f;border-color:#d6e9c6');
+    }
+    Logger.warning = function() {
+        console.log(parse(arguments), 'font-weight: bold;border-radius:3px;/*background:#fcf8e3*/;color: #8a6d3b;border-color:#faebcc');
+    }
+    Logger.danger = function() {
+        console.log(parse(arguments), 'font-weight: bold;border-radius:3px;/*background:#f2dede*/;color: #a94442;border-color:#ebccd1');
+    }
+
+}());
